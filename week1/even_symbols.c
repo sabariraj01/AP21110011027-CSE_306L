1434b94ff40be8f5ad9467738fcd65296fafdcec
Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-08-13 21:48:41 +0530

    Update even_symbols.c

--- week1/even_symbols.c
+++ week1/even_symbols.c
@@ -1,3 +1,5 @@
+//Implementation of Language recognizer for set of all strings over input alphabet ∑={a,b} containing even number of a’s and even number of b’s.
+
 #include<stdio.h>
 #include <stdlib.h>
 int main()
